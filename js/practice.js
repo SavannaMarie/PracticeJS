4011f86ef33236cd9be2decedbbe7077813a4dd9
Savanna Marie <savannamarie@gmail.com>
2021-04-21 20:44:39 -0500

    two exercises

--- js/practice.js
+++ js/practice.js
@@ -3,3 +3,23 @@
 --------------------------------------------
 
  */
+
+
+// Return true if one of the numbers is 100
+// or if the sum of the two numbers is 100
+
+function numberCheck(x, y) {
+    if (x === 100 || y === 100 || x + y === 100) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+// Write a JS program to get the extension
+// of a filename
+
+function getFileName(str){
+    return str.slice(str.lastIndexOf("."));
+}
