c3f926b6b9a345a34691588c48e1ad25b81cccf6
Savanna Marie <savannamarie@gmail.com>
2021-10-05 10:24:55 -0500

    Javascript

--- js/practice.js
+++ js/practice.js
@@ -115,3 +115,37 @@ function isEmptyString(value) {
 
 
 
+"use strict";
+
+// Don't change the next two lines!
+// This creates two variables:
+//     one with the colors of the rainbow, and another with a single randome
+//     another with a single random color value
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var color = colors[Math.floor(Math.random()*colors.length)];
+
+switch (color) {
+    case "red":
+        console.log("You are red");
+        break;
+    case "orange":
+        console.log("You are orange");
+        break;
+    case "yellow":
+        console.log("You are yellow");
+        break;
+    case "green":
+        console.log("You are green");
+        break;
+    case "blue":
+        console.log("You are blue");
+        break;
+    default:
+        console.log("Color is indigo or violet");
+}
+// TODO: create a case statement that will handle every color except indigo and violet
+// TODO: when a color is encountered log a message that tells the color, and an object of that color
+//       example: Blue is the color of the sky.
+
+// TODO: create a default case that will catch indigo and violet
+// TODO: for the default case, log: I do not know anything by that color.
